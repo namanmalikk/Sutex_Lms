d9c2c63b743cc0f9e8297a9b1a752167c637fd42
Naman Malik <namanmalikk@gmail.com>
2020-09-19 23:03:45 +0530

    separate login for student and teachers

--- courses/views.py
+++ courses/views.py
@@ -2,23 +2,53 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import Group
 from .forms import CreateUserForm
 
 
-def signupUser(request):
+def signupStudent(request):
     form = CreateUserForm()
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            group = Group.objects.get(name='student')
+            user.groups.add(group)
             messages.success(request, 'user succesfully created')
-            return redirect('loginUser')
+            return redirect('home')
 
     context = {'form': form}
     return render(request, 'signup.html', context)
 
 
-def loginUser(request):
+def loginStudent(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'logged in')
+    context = {}
+    return render(request, 'login.html', context)
+
+
+def signupTeacher(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            group = Group.objects.get(name='teacher')
+            user.groups.add(group)
+            messages.success(request, 'user succesfully created')
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'signup.html', context)
+
+
+def loginTeacher(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -33,7 +63,7 @@ def loginUser(request):
 def logoutUser(request):
     logout(request)
     messages.success(request, 'logged out')
-    return redirect('loginUser')
+    return redirect('home')
 
 
 def home(request):
